069f2b347eb04d4b08538e874d614c7d4e48236b
ayeleworkm <ayeleworkm@berea.edu>
2024-08-26 11:44:58 -0400

    Update main.cpp
    Added name to project

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
     Edits made by:
     TODO: <Baella Morgan> -- to be added by student 1
     TODO: <Add name of student 2 Here> -- to be added by student 2
-    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+    TODO: <Mathewos> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
  */
 #include <iostream>
 using namespace std;
