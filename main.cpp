ecc89183f11f4138eae3d28f47645fc63e8baa63
BC-Langston <156454144+BC-Langston@users.noreply.github.com>
2024-08-28 11:35:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 /*  T02: Collaboration Teamwork
 
-    Have yov ever heard of rubber duck debugging?
+    Have you ever heard of rubber duck debugging?
     See https://en.wikipedia.org/wiki/Rubber_duck_debugging 
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-     <Baella Morgan> -- to be added by student 1
-     Langston Hill -- to be added by student 2
-     <Mathewos> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+<<<<<<< Updated upstream
+    TODO: <Baella Morgan> -- to be added by student 1
+    TODO: <Add name of student 2 Here> -- to be added by student 2
+    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+=======
+     <Baella Morgan> 
+     Langston Hill 
+     <Mathewos> --
+>>>>>>> Stashed changes
  */
 #include <iostream>
 using namespace std;
@@ -22,10 +28,10 @@ int main() {
     cin >> ducknum;
 
     for (int i = 0; i < ducknum; i++) { 
-        cout << "                /\    " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
+        cout << "                /\    " << endl; // student 1 may change this line and may add additional lines immediately above
         cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
-        cout << "                `----' " << endl;// TODO student 2 may change this line and may add lines below
+        cout << "                `----' " << endl; // student 2 may change this line and may add lines below
         cout << "            *Squeaky!* " << endl;
     }
 
